888aa4a07bd0f0ec5ea655194d876a0cbe6158b9
Wakoya Daba <waaqoyyaadhaabaa@gmail.com>
2025-11-17 21:37:43 +0300

    Update script.js

--- Desktop/shaakala/accor/script.js
+++ Desktop/shaakala/accor/script.js
@@ -1,6 +1,5 @@
 const questions = document.querySelectorAll('.question');
     questions.forEach(question => {
-        
       question.addEventListener('click', () => {
         question.parentElement.classList.toggle('active');
       });
